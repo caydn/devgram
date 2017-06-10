691fb3a30ee207b35216e8ce099f7fe492effe6d
Cayden Jacobs <cayden.jacobs@yahoo.com>
2017-06-10 00:02:27 -0400

    added comment functionality to each post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
   validates :user_id, presence: true
   validates_presence_of :image
   belongs_to :user
+  has_many :comments, dependent: :destroy
   mount_uploader :image, PhotoUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608052846) do
+ActiveRecord::Schema.define(version: 20170608064800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "image"
@@ -43,5 +53,7 @@ ActiveRecord::Schema.define(version: 20170608052846) do
     t.index ["user_name"], name: "index_users_on_user_name", unique: true, using: :btree
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
